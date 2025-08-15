d4f6ac1c1635ad9ff105451e9dd0cff257c6f400
Barradas13 <febarradas13@gmail.com>
2025-08-15 09:39:31 -0300

    tirando CORS

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 import express from "express"
 import { router } from "./router/router.js";
-import cors from 'cors';
+import path from "path";
+import { fileURLToPath } from "url";
+
+//import cors from 'cors';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+//app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use("/", router);
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
