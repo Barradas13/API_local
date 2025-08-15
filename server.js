59e34e87967ac3bf951ad8aaeaf3aff3b10f4550
Barradas13 <febarradas13@gmail.com>
2025-08-15 09:46:09 -0300

    ...

--- server.js
+++ server.js
@@ -3,16 +3,12 @@ import { router } from "./router/router.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
-//import cors from 'cors';
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
